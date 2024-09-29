ca4ae4434b41eea6da5065c2008b21fa1da48f16
Frederick-c0der <frederickchanngyan@gmail.com>
2024-09-29 10:56:35 +0100

    Add files via upload

--- Quiz.py
+++ Quiz.py
@@ -11,8 +11,12 @@ skipbox=Rect(630,250, 125, 225)
 allquestions=[]
 currentquestion=0
 numofquestion=0
+Score=0
+boxes=[box1,box2,box3,box4]
+Time=60
 question=[]
 HEIGHT=500
+gamefin=False
 def draw():
     screen.fill("black")
     screen.draw.filled_rect(r2, "black")
@@ -25,12 +29,12 @@ def draw():
     screen.draw.filled_rect(skipbox, "dark green")
     
     screen.draw.textbox(question[0], qbox, color="white")
-    screen.draw.textbox("Question", timebox, color="white")
+    screen.draw.textbox(str(Time), timebox, color="white")
     screen.draw.textbox(question[1], box1, color="black")
     screen.draw.textbox(question[2], box2, color="black")
     screen.draw.textbox(question[3], box3, color="black")
     screen.draw.textbox(question[4], box4, color="black")
-    screen.draw.textbox("Question", r2, color="white")
+    screen.draw.textbox("Answer the question to win the game", r2, color="white")
     screen.draw.textbox("Skip", skipbox, color="black", angle=90)
 def readquestions():
     global allquestions
@@ -41,8 +45,43 @@ def readquestions():
 def ifansweredcorrectly():
     global currentquestion
     global question
-    question=allquestions[currentquestion].split(",")
-    print(question)
+    if currentquestion<numofquestion:
+        question=allquestions[currentquestion].split(",")
+    else:
+        gameover()
+def on_mouse_down(pos):
+    global currentquestion
+    global Score
+    if gamefin==False:
+        if boxes[int(question[5])-1].collidepoint(pos):
+            currentquestion+=1
+            Score+=1
+            ifansweredcorrectly()
+        elif skipbox.collidepoint(pos):
+            currentquestion+=1
+            ifansweredcorrectly()
+        else:
+            gameover()
+    else:
+        gameover()
+def gameover():
+    global question
+    global gamefin
+    question=["Game over", "-", "-", "-", "-", "5"]
+    gamefin=True
+def timer():
+    global Time
+    global gamefin
+    if gamefin==False:
+        if Time>0:
+            Time-=1
+        else:
+            gameover()
+clock.schedule_interval(timer, 1)
+def update():
+    r2.x-=2
+    if r2.x==-800:
+        r2.x=1000
 readquestions()
 ifansweredcorrectly()
 pgzrun.go()
